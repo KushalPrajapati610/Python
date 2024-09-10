925e0837acccfe337e237db7d2dd8d94338d1d3d
KushalPrajapati610 <kushalprajapati613@gmail.com>
2024-09-10 17:50:51 +0530

    Basic

--- mypython/IfelsePRograms/p58 Accept the electric units from the user and calculate the bill according to the following rates.py
+++ mypython/IfelsePRograms/p58 Accept the electric units from the user and calculate the bill according to the following rates.py
@@ -2,7 +2,7 @@ units=int(input("Enter the number of units ="))
 
 if units < 100:
     print("Free")
-elif units > 100 and units < 300:
+elif 100 < units < 300:
     print("units =", units * 2)
 elif units > 300:
     print("units =", (units -300) * 5)
---
+++ mypython/loop/For Loop/p60loop1.py
@@ -0,0 +1,3 @@
+n = int(input("Enter the limits ="))
+for i in range(1, n):
+    print("Hi", i)
